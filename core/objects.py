da7e13a594b37392070416ca348d38539e2db028
dashygo097 <dashygo097@gmail.com>
2025-04-12 18:55:04 +0800

    del bvh

--- core/objects.py
+++ core/objects.py
@@ -1,7 +1,6 @@
 import taichi as ti
 from taichi.math import vec3
 
-from .bvh import AABB
 from .records import HitInfo
 from .utils.const import EPSILON, FAR_Z, NEAR_Z, TMAX, TMIN
 
@@ -22,29 +21,11 @@ class Triangle:
     v0: vec3  # pyright: ignore
     v1: vec3  # pyright: ignore
     v2: vec3  # pyright: ignore
-    bbox: AABB  # pyright: ignore
 
     albedo: vec3  # pyright: ignore
     metallic: ti.f32  # pyright: ignore
     roughness: ti.f32  # pyright: ignore
     emission: vec3  # pyright: ignore
-
-    def init(self):
-        v0 = self.v0
-        v1 = self.v1 + self.v2
-        self.bbox.from_vec(v0, v1)
-        if self.bbox.x.min >= self.bbox.x.max:
-            self.bbox.x.min -= NEAR_Z
-            self.bbox.x.max += FAR_Z
-            print(self.bbox.x.min, self.bbox.x.max)
-        if self.bbox.y.min >= self.bbox.y.max:
-            self.bbox.y.min -= NEAR_Z
-            self.bbox.y.max += FAR_Z
-            print(self.bbox.y.min, self.bbox.y.max)
-        if self.bbox.z.min >= self.bbox.z.max:
-            self.bbox.z.min -= NEAR_Z
-            self.bbox.z.max += FAR_Z
-            print(self.bbox.z.min, self.bbox.z.max)
 
     @ti.func
     def intersect(self, ray: Ray, tmin=TMIN, tmax=TMAX):  # pyright: ignore
@@ -111,18 +92,11 @@ class Sphere:
     tag: ti.i32  # pyright: ignore
     center: vec3  # pyright: ignore
     radius: float  # pyright: ignore
-    bbox: AABB  # pyright: ignore
 
     albedo: vec3  # pyright: ignore
     metallic: ti.f32  # pyright: ignore
     roughness: ti.f32  # pyright: ignore
     emission: vec3  # pyright: ignore
-
-    def init(self):
-        self.bbox.from_vec(
-            self.center - vec3(self.radius),
-            self.center + vec3(self.radius),
-        )
 
     @ti.func
     def intersect(self, ray: Ray, tmin=TMIN, tmax=TMAX):  # pyright: ignore
@@ -183,41 +157,3 @@ class Sphere:
 class DirecLight:
     dir: vec3  # pyright: ignore
     color: vec3  # pyright: ignore
-
-
-def init4bbox(obj):
-    if isinstance(obj, Triangle):
-        init_triangle(obj)
-    elif isinstance(obj, Sphere):
-        init_sphere(obj)
-    else:
-        raise ValueError("Unknown object type")
-
-
-# NOTE: I HAVE NO IDEA WHY MY ORIGINAL CODE DOESN'T WORK
-# LEAVE THIS FOR TEMPORARY USE
-def init_triangle(triangle):
-    triangle.bbox.x.min = ti.min(triangle.v0.x, triangle.v1.x, triangle.v2.x)
-    triangle.bbox.x.max = ti.max(triangle.v0.x, triangle.v1.x, triangle.v2.x)
-    triangle.bbox.y.min = ti.min(triangle.v0.y, triangle.v1.y, triangle.v2.y)
-    triangle.bbox.y.max = ti.max(triangle.v0.y, triangle.v1.y, triangle.v2.y)
-    triangle.bbox.z.min = ti.min(triangle.v0.z, triangle.v1.z, triangle.v2.z)
-    triangle.bbox.z.max = ti.max(triangle.v0.z, triangle.v1.z, triangle.v2.z)
-    if triangle.bbox.x.min >= triangle.bbox.x.max:
-        triangle.bbox.x.min -= 2 * TMIN
-        triangle.bbox.x.max += 2 * TMIN
-    if triangle.bbox.y.min >= triangle.bbox.y.max:
-        triangle.bbox.y.min -= 2 * TMIN
-        triangle.bbox.y.max += 2 * TMIN
-    if triangle.bbox.z.min >= triangle.bbox.z.max:
-        triangle.bbox.z.min -= 2 * TMIN
-        triangle.bbox.z.max += 2 * TMIN
-
-
-def init_sphere(sphere):
-    sphere.bbox.x.min = sphere.center.x - sphere.radius
-    sphere.bbox.x.max = sphere.center.x + sphere.radius
-    sphere.bbox.y.min = sphere.center.y - sphere.radius
-    sphere.bbox.y.max = sphere.center.y + sphere.radius
-    sphere.bbox.z.min = sphere.center.z - sphere.radius
-    sphere.bbox.z.max = sphere.center.z + sphere.radius
